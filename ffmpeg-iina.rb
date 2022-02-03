7dc390ebf4fcba6dd566feb938938b16ee63f4e8
Carter Li <zhangsongcui3371@sina.com>
2022-02-03 13:16:41 +0800

    Build for macOS 10.15

--- ffmpeg-iina.rb
+++ ffmpeg-iina.rb
@@ -4,8 +4,8 @@
 class FfmpegIina < Formula
   desc "Play, record, convert, and stream audio and video"
   homepage "https://ffmpeg.org/"
-  url "https://ffmpeg.org/releases/ffmpeg-4.4.1.tar.xz"
-  sha256 "eadbad9e9ab30b25f5520fbfde99fae4a92a1ae3c0257a8d68569a4651e30e02"
+  url "https://ffmpeg.org/releases/ffmpeg-5.0.tar.xz"
+  sha256 "51e919f7d205062c0fd4fae6243a84850391115104ccf1efc451733bc0ac7298"
   head "https://github.com/FFmpeg/FFmpeg.git"
 
   keg_only <<EOS
@@ -16,21 +16,8 @@ EOS
   depends_on "nasm" => :build
   depends_on "pkg-config" => :build
   depends_on "dav1d"
-  depends_on "fontconfig"
   depends_on "freetype"
-  depends_on "frei0r"
-  depends_on "gnutls"
-  depends_on "libass"
-  depends_on "libbluray"
-  depends_on "libsoxr"
-  depends_on "libvidstab"
-  depends_on "rubberband"
-  depends_on "snappy"
-  depends_on "speex"
-  depends_on "tesseract"
   depends_on "xz"
-  depends_on "zeromq"
-  depends_on "zimg"
 
   uses_from_macos "bzip2"
   uses_from_macos "libxml2"
@@ -46,25 +33,12 @@ EOS
       --cc=#{ENV.cc}
       --host-cflags=#{ENV.cflags}
       --host-ldflags=#{ENV.ldflags}
-      --enable-ffplay
-      --enable-gnutls
       --enable-gpl
-      --enable-libbluray
+      --enable-lto
       --enable-libdav1d
-      --enable-librubberband
-      --enable-libsnappy
-      --enable-libtesseract
-      --enable-libvidstab
-      --enable-libxml2
-      --enable-libfontconfig
-      --enable-libfreetype
-      --enable-frei0r
-      --enable-libass
-      --enable-libspeex
-      --enable-libsoxr
       --enable-videotoolbox
-      --enable-libzmq
-      --enable-libzimg
+      --disable-debug
+      --disable-doc
       --disable-libjack
       --disable-indev=jack
       --disable-programs
