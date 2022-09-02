8169e0d510d6d965ca39fe07cc63a621653b2705
李通洲 <carter.li@eoitek.com>
2022-09-02 16:22:18 +0800

    ffmpeg: update to 5.1.1

--- ffmpeg-iina.rb
+++ ffmpeg-iina.rb
@@ -4,8 +4,8 @@
 class FfmpegIina < Formula
   desc "Play, record, convert, and stream audio and video"
   homepage "https://ffmpeg.org/"
-  url "https://ffmpeg.org/releases/ffmpeg-5.1.tar.xz"
-  sha256 "55eb6aab5ee235550fa54a33eaf8bf1b4ec66c01453182b12f6a993d75698b03"
+  url "https://ffmpeg.org/releases/ffmpeg-5.1.1.tar.xz"
+  sha256 "95bf3ff8c496511e71e958fb249e663c8c9c3de583c5bebc0f5a9745abbc0435"
   head "https://github.com/FFmpeg/FFmpeg.git"
 
   keg_only <<EOS
